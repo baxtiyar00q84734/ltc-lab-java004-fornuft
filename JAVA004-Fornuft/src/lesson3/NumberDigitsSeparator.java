7e7b2843fe3f349d15f20101b5809dcbc8e5edc8
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-02 13:26:02 +0400

    Number digits separator

---
+++ JAVA004-Fornuft/src/lesson3/NumberDigitsSeparator.java
@@ -0,0 +1,33 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class NumberDigitsSeparator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();//7488
+        int reversedN = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            reversedN = reversedN * 10 + digit;
+            n /= 10;
+
+        }
+
+        // System.out.println(reversedN);
+
+        boolean isFirstDigit = true;
+
+        while (reversedN > 0) {
+            if (!isFirstDigit) {
+                System.out.print("-");
+            }
+            int temp = reversedN % 10;
+            reversedN /= 10;
+            System.out.print(temp);
+            isFirstDigit = false;
+        }
+    }
+}
