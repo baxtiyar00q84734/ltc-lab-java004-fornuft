11a920a42c0705605f0fa1181dd0613ae59e9360
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-05 20:34:48 +0400

    lesson 4 ders izahi materiallar

---
+++ JAVA004-Fornuft/src/lesson4/Main.java
@@ -0,0 +1,53 @@
+package lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int marriageAnniversary = 2;
+//
+//        if (marriageAnniversary == 1) {
+//            System.out.println("It is paper anniversary");
+//        } else if (marriageAnniversary == 2) {
+//            System.out.println("It is cotton anniversary");
+//        } else if (marriageAnniversary == 3) {
+//            System.out.println("It is leather anniversary");
+//        }
+
+//        switch (marriageAnniversary) {
+//            case 1:
+//                System.out.println("Paper anniversary");
+//                break;
+//            case 2:
+//                System.out.println("Cotton anniversary");
+//                break;
+//            case 3:
+//                System.out.println("Leather anniversary");
+//                break;
+//            default:
+//                System.out.println("Not such a case");
+//
+
+//        boolean isMarried = true;
+//        String response = "";
+//
+//        response = isMarried ? "Evli" : "Subay";// ? true : false
+//
+//        System.out.println(response);
+
+//        int age = 15;
+//        String icaze = "";
+//        icaze = age > 18 ? "Giris mumkundur" : "Eks halda qadagandir";
+//
+//        System.out.println(icaze);
+
+
+
+        
+        int yanacaqSerfiyati = 11;
+
+        String cavab = yanacaqSerfiyati > 10 ? "Cox yandirir" : " Az yandirir";
+
+        System.out.println(cavab);
+    }
+}
+
