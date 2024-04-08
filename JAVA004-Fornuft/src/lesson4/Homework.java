c36c2d641f7d336d3f67b40ace80a128b9dcdb3e
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-08 15:28:04 +0400

    lesson 4 homework
    switch case grade letter

---
+++ JAVA004-Fornuft/src/lesson4/Homework.java
@@ -0,0 +1,37 @@
+package lesson4;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your exam score: ");
+        int score = scanner.nextInt();
+
+        String grade = null;
+        if (score >= 0 && score <= 100) {
+            switch (score / 10) {
+                case 10:
+                case 9:
+                    grade = "A";
+                    break;
+                case 8:
+                    grade = "B";
+                    break;
+                case 7:
+                    grade = "C";
+                    break;
+                case 6:
+                    grade = "D";
+                    break;
+                default:
+                    grade = "F";
+            }
+        } else {
+            System.out.println("there is no letter for this grade");
+        }
+
+
+        System.out.println("Your grade's letter is: " + grade);
+    }
+}
