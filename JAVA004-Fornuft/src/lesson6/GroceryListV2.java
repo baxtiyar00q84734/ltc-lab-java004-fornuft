9d7602bfe202aaab0bd87bdb564cb56d59dd5926
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-19 16:33:41 +0400

    GroceryList(with ArrayList)

---
+++ JAVA004-Fornuft/src/lesson6/GroceryListV2.java
@@ -0,0 +1,31 @@
+package lesson6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GroceryListV2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int itemCount = 1;
+        ArrayList<String> groceryList = new ArrayList<>();
+
+        System.out.println("Enter the items for your grocery list. Type 'DONE' when you are finished:");
+
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if ("DONE".equalsIgnoreCase(input)) {
+                break;
+            }
+
+            groceryList.add(input);
+        }
+
+        System.out.println("\nYour grocery list:");
+        for (String item : groceryList) {
+            System.out.println(item);
+        }
+    }
+}
