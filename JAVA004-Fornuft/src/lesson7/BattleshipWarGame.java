8c4a198773874ee1e7db95f3ff7a0293bc9da892
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-20 18:27:50 +0400

    updates on BWG adding error message and visualize after game over

--- JAVA004-Fornuft/src/lesson7/BattleshipWarGame.java
+++ JAVA004-Fornuft/src/lesson7/BattleshipWarGame.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class BattleshipWarGame {
     public static void main(String[] args) {
 
+// 0- empty and not checked  1- full not checked 2- full and checked 3-empty checked
+
         int[][] battleshipMap = {
                 {0, 0, 1, 1, 1, 1, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
@@ -33,17 +35,31 @@ public class BattleshipWarGame {
             int userHitRow = scanner.nextInt();
             int userHitCol = scanner.nextInt();
 
+            if (userHitRow < 0 || userHitRow >= 10 || userHitCol < 0 || userHitCol >= 10) {
+                System.out.println("Coordinates out of bounds. Rows and columns must be between 0 and 9.");
+                continue;
+            }
+
             if (battleshipMap[userHitRow][userHitCol] == 1) {
                 System.out.println("You hit!");
-                
+
                 battleshipMap[userHitRow][userHitCol] = 2;
             } else {
                 System.out.println("No hit!");
+
+                battleshipMap[userHitRow][userHitCol] = 3;
             }
 
             System.out.println("Do you want to continue? (type 'quit' to stop)");
             String finishGame = scanner.next();
             if ("quit".equals(finishGame)) {
+
+                for (int i = 0; i < battleshipMap.length; i++) {
+                    for (int j = 0; j < battleshipMap[i].length; j++) {
+                        System.out.print(battleshipMap[i][j] + " ");
+                    }
+                    System.out.println();
+                }
                 break;
             }
         }
