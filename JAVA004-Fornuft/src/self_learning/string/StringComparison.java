31e6867694a373053b0e35ce8e6bc8091787463d
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-25 12:59:19 +0400

    (s_l) StringComparison

--- JAVA004-Fornuft/src/self_learning/string/StringComparison.java
+++ JAVA004-Fornuft/src/self_learning/string/StringComparison.java
@@ -2,13 +2,25 @@ package self_learning.string;
 
 public class StringComparison {
     public static void main(String[] args) {
-        String str1 = "hello";
-        String str2 = "hello";
-        if (str1 == str2) {
-            System.out.println("Strings are equal");
-        } else {
-            System.out.println("Strings are not equal");
-        }
+        System.out.println("===== String comparison =====");
+        System.out.println();
+
+        String s = " hello ";
+        String s2 = " hello ";
+        System.out.println("s == s2: " + (s == s2));					// true
+
+        String s3 = new String(" hello ");
+        System.out.println("s == s3: " + (s == s3));					// false
+        System.out.println("s.equals(s3): " + s.equals(s3)); 			// true
+        System.out.println("s == s3.intern(): " + (s == s3.intern()));	// true
+
+        String firstName = "Andrii";
+        String firstName2 = "andrii";
+        System.out.println("firstName.equals(firstName2): "
+                + firstName.equals(firstName2));			// false
+        System.out.println("firstName.equalsIgnoreCase(firstName2): "
+                + firstName.equalsIgnoreCase(firstName2)); 	// true
+
 
     }
 }
