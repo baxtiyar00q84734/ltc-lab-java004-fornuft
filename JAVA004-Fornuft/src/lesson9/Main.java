8570be64d1d716c60e35bdbc20adc83a3bd2a688
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-27 14:30:40 +0400

    Book and Author classes are finished

---
+++ JAVA004-Fornuft/src/lesson9/Main.java
@@ -0,0 +1,15 @@
+package lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+        Author author1=new Author("Elxan Elatlı",1962);
+        Author author2=new Author("Attilâ İlhan",1925);
+        Book book1=new Book("Ben Sana Mecburum",author2,1960);
+        Book book2=new Book("Niyə Susursan, Polkovnik?",author1,2014);
+        Book book3=new Book("Bakıdan Gələn Xəfiyyə",author1,2011);
+
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+    }
+}
