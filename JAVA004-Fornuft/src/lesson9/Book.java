f98bffb4aa828f149d719fa4037e10ea5fdb40f3
BakhtiyarGuluzada <q.baxtiyar07@gmail.com>
2024-04-29 19:12:46 +0400

    updates for Book and Author classes

--- JAVA004-Fornuft/src/lesson9/Book.java
+++ JAVA004-Fornuft/src/lesson9/Book.java
@@ -11,6 +11,30 @@ public class Book {
         this.publicationYear = publicationYear;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
--- JAVA004-Fornuft/src/lesson9/Main.java
+++ JAVA004-Fornuft/src/lesson9/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Author author1=new Author("Elxan Elatlı",1962);
         Author author2=new Author("Attilâ İlhan",1925);
-        Book book1=new Book("Ben Sana Mecburum",author2,1960);
+        Book book1=new Book("Ben Sana Mecburum",author2,1966);
         Book book2=new Book("Niyə Susursan, Polkovnik?",author1,2014);
         Book book3=new Book("Bakıdan Gələn Xəfiyyə",author1,2011);
+
+        book1.setPublicationYear(1960);
 
         System.out.println(book1);
         System.out.println(book2);
